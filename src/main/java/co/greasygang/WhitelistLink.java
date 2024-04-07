343d9d28c37c9c0cf9f8734bd589fc3d296870f0
dargy <dragonismbot@protonmail.com>
2024-04-07 00:14:32 -0500

    feat: add requesting codes and whitelist

--- src/main/java/co/greasygang/WhitelistLink.java
+++ src/main/java/co/greasygang/WhitelistLink.java
@@ -5,11 +5,12 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Whitelistlink implements ModInitializer {
+public class WhitelistLink implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("whitelist-link");
+	public static final String MOD_ID = "whitelistlink";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
