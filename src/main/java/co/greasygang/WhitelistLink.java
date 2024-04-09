bc382643f23f933de9bca6001065e1a32574e9d6
veryCrunchy <verycrunchydev@gmail.com>
2024-04-10 00:02:16 +0200

    chore: improve toggle statusmessage

--- src/main/java/co/greasygang/WhitelistLink.java
+++ src/main/java/co/greasygang/WhitelistLink.java
@@ -11,7 +11,7 @@ import static net.minecraft.server.command.CommandManager.literal;
 
 public class WhitelistLink implements ModInitializer {
 	public static final String MOD_ID = "whitelistlink";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -21,29 +21,30 @@ public class WhitelistLink implements ModInitializer {
 		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, environment) -> {
 			// Reload command
 			dispatcher.register(literal("whitelistlink")
-				.executes(context -> {
-					context.getSource().sendFeedback(() -> Text.literal("§6WhitelistLink §eby VeryCrunchy, dargy\n§7Connects the greasygang.co whitelist to your Minecraft server"), false);
-					return 1;
-				})
-				.then((literal("reload")
-					.requires(source -> source.hasPermissionLevel(4))
 					.executes(context -> {
-						WhitelistLinkConfig.init();
-						context.getSource().sendFeedback(() -> Text.literal("§6WhitelistLink config reloaded."), false);
+						context.getSource().sendFeedback(() -> Text.literal(
+								"§6WhitelistLink §eby VeryCrunchy, dargy\n§7Connects the greasygang.co whitelist to your Minecraft server"),
+								false);
 						return 1;
-					})))
-				.then((literal("toggle")
-					.executes(context -> {
-						WhitelistLinkConfig.toggleEnabled();
-
-						if (WhitelistLinkConfig.isEnabled()) {
-							context.getSource().sendFeedback(() -> Text.literal("§aWhitelistLink is now enabled."), false);
-						} else {
-							context.getSource().sendFeedback(() -> Text.literal("§cWhitelistLink is now disabled."), false);
-						}
-						return 1;
-					})))
-			);
+					})
+					.then((literal("reload")
+							.requires(source -> source.hasPermissionLevel(4))
+							.executes(context -> {
+								WhitelistLinkConfig.init();
+								context.getSource().sendFeedback(() -> Text.literal("§6WhitelistLink config reloaded."),
+										false);
+								return 1;
+							})))
+					.then((literal("toggle")
+							.executes(context -> {
+								WhitelistLinkConfig.toggleEnabled();
+								String statusMessage = String.format(
+										"§%sWhitelistLink is now %s.",
+										WhitelistLinkConfig.isEnabled() ? "a" : "c",
+										WhitelistLinkConfig.isEnabled() ? "enabled" : "disabled");
+								context.getSource().sendFeedback(() -> Text.literal(statusMessage), false);
+								return 1;
+							}))));
 		});
 	}
 }
